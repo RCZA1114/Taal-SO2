1cc8e8b6e9666604ecb9b57216289aa6fe8c4f98
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-10-31 10:55:04 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title("Taal Volcano SO2 Readings")
 
 
-df = pd.read_csv("Taal Updated.csv")
+df = pd.read_csv("Taal SO2.csv")
 df['Date'] = pd.to_datetime(df['Date'])
 df['Month'] = df['Date'].dt.month
 df['Day'] = df['Date'].dt.day
@@ -17,6 +17,6 @@ selected_years = st.multiselect('Select year(s)', options = years, default=years
 
 filter = df[df['Year'].isin(selected_years)]
 
-fig = px.line(filter, x='Date', y='SO2', title="Chart of the Data",)
+fig = px.line(filter, x='Date', y='SO2 Flux', title="Chart of the Data",)
 
 st.plotly_chart(fig, use_container_width=True)
